a39e4b402655e2a901f0461f92881b7a81e75023
Matheus Cordeiro <matheuscordeirop@gmail.com>
2023-08-24 13:24:27 -0300

    CategoryItem and ProductItem components added

---
+++ src/components/CategoryItem.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import { StyleSheet, Text, View, Image, TouchableOpacity } from 'react-native';
+
+const CategoryItem = ({ item, onPressItem }) => (
+  <View style={styles.categoryContainer}>
+    <TouchableOpacity onPress={() => onPressItem(item)}>
+      <Image
+        source={require('../assets/category-default.png')}
+        style={styles.categoryImage}
+      />
+      <Text style={styles.categoryName}>{item}</Text>
+    </TouchableOpacity>
+  </View>
+);
+
+const styles = StyleSheet.create({
+  categoryContainer: {
+    width: '30%',
+    alignItems: 'center',
+    marginVertical: 10,
+  },
+  categoryImage: {
+    width: '100%',
+    height: 110,
+    marginBottom: 5,
+  },
+  categoryName: {
+    textAlign: 'center',
+  },
+});
+
+export default CategoryItem;
---
+++ src/components/ProductItem.tsx
@@ -0,0 +1,50 @@
+import React from 'react';
+import { StyleSheet, Text, View, Image, TouchableOpacity } from 'react-native';
+
+const ProductItem = ({ item, onPressItem }) => {
+  return (
+    <TouchableOpacity style={styles.productContainer} onPress={onPressItem}>
+      <View style={styles.productImageContainer}>
+        <Image source={{ uri: item.image }} style={styles.productImage} />
+      </View>
+      <Text style={styles.productName} numberOfLines={3}>
+        {item.title}
+      </Text>
+      <Text style={styles.productPrice}>${item.price.toFixed(2)}</Text>
+    </TouchableOpacity>
+  );
+};
+
+const styles = StyleSheet.create({
+  productContainer: {
+    backgroundColor: '#ffffff',
+    borderRadius: 8,
+    padding: 10,
+    marginBottom: 15,
+    elevation: 3,
+    width: '45%',
+    marginLeft: '3.5%',
+  },
+  productImageContainer: {
+    alignItems: 'center',
+    marginBottom: 10,
+  },
+  productImage: {
+    width: 80,
+    height: 80,
+    resizeMode: 'cover',
+    borderRadius: 8,
+  },
+  productName: {
+    fontSize: 14,
+    fontWeight: 'bold',
+    marginBottom: 5,
+  },
+  productPrice: {
+    fontSize: 14,
+    fontWeight: 'bold',
+    color: '#15970d',
+  },
+});
+
+export default ProductItem;
